2bf28f3d9ace6465ec79a3de3e68a19a280c6de2
Matthew Hartman <msh1011@wildcats.unh.edu>
2020-02-05 10:28:24 -0500

    fixed path

--- functions/tile_api_function/tile_api_function.py
+++ functions/tile_api_function/tile_api_function.py
@@ -1,16 +1,19 @@
 import os
 import boto3
+import base64
+
 
 dynamodb = boto3.client('dynamodb')
 s3_client = boto3.client("s3")
 DATA_TABLE = os.getenv('DATA_TABLE')
 DATA_BUCKET = os.getenv('DATA_BUCKET')
 
+
 def lambda_handler(event, context):
-    region = event["pathParameters"]["region"] 
+    region = event["pathParameters"]["region"]
     t = event["pathParameters"]["t"]
-    z = event["pathParameters"]["z"],
-    x = event["pathParameters"]["x"],
+    z = event["pathParameters"]["z"]
+    x = event["pathParameters"]["x"]
     y = os.path.splitext(event["pathParameters"]["y"])[0]
     table_index = "{}-{}-{}-{}-{}".format(region, t, z, x, y)
     res = dynamodb.get_item(
@@ -34,6 +37,12 @@ def lambda_handler(event, context):
     else:
         data = res["Item"]["tile"]["B"]
     return {
-        'statusCode': 200,
-        'body': data
+        "isBase64Encoded": True,
+        "statusCode": 200,
+        "headers": {
+            "content-type": "application/x-protobuf",
+            "content-encoding": "gzip",
+        },
+        "body":  base64.b64encode(data).decode("utf-8"),
+        "Access-Control-Allow-Origin": "*"
     }
