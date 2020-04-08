c135bae7a1f90cac6598bad17558ed7760f91396
Matthew Hartman <msh1011@wildcats.unh.edu>
2020-04-08 10:11:12 -0400

    added fix to json

--- functions/json2mvt/mbutil.py
+++ functions/json2mvt/mbutil.py
@@ -2,6 +2,8 @@ import sqlite3
 import sys
 import logging
 import os
+import json
+import math
 import boto3
 
 """
@@ -10,12 +12,12 @@ Code Adapted from MBUTIL
 Modified into a cloud-native SQL Egress via Lambda to AWS DynamoDB
 """
 
-dynamodb = boto3.resource("dynamodb")
-dynamodb_table = dynamodb.Table(os.getenv("DATA_TABLE"))
-huge_bucket = os.getenv("DATA_BUCKET")
-s3 = boto3.client("s3")
-
-logger = logging.getLogger(__name__)
+#dynamodb = boto3.resource("dynamodb")
+#dynamodb_table = dynamodb.Table(os.getenv("DATA_TABLE"))
+#huge_bucket = os.getenv("DATA_BUCKET")
+#s3 = boto3.client("s3")
+#
+#logger = logging.getLogger(__name__)
 
 
 def flip_y(zoom, y):
@@ -68,10 +70,67 @@ def mbtiles_to_disk(mbtiles_file, loc, update_time, **kwargs):
             else:
                 print("Miss:", key, len(t[3]))
                 s3.put_object(
-                    Bucket=huge_bucket,
-                    Key=key,
-                    Body=t[3])
+                        Bucket=huge_bucket,
+                        Key=key,
+                        Body=t[3])
                 entry["tile"] = str.encode("a") #need non-null
                 batch.put_item(Item=entry)
 
             t = tiles.fetchone()
+def angle3pt(a, b, c):
+    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
+    return ang + 360 if ang < 0 else ang
+
+
+def json_filter(obj):
+    new_obj = {"type": "FeatureCollection", "bbox": [], "features": []}
+
+    new_obj["bbox"] = obj["bbox"]
+    for x in range(0, len(obj["features"])):
+        feat = {
+                "type": "Feature",
+                "id": x,
+                "properties": {},
+                "geometry": {"type": "LineString", "properties": [], "coordinates": []},
+                }
+        if len(obj["features"][x]["geometry"]["coordinates"]) <= 4:
+            continue
+        nline = []
+        nProps = {
+                "index": obj["features"][x]["properties"]["index"],
+                "streamline_level": obj["features"][x]["properties"]["streamline_level"],
+                "seed_index": obj["features"][x]["properties"]["seed_index"],
+                "point_levels":[],
+                "magnitudes":[],
+                "directions": [],
+                "dSep": obj["features"][x]["properties"]["dSep"],
+                "iSteps": obj["features"][x]["properties"]["iSteps"]
+                }
+        skip = False
+        m1 = len(obj["features"][x]["geometry"]["coordinates"]) - 2
+        for y in range(0, m1):
+            cords = obj["features"][x]["geometry"]["coordinates"][y]
+            cnext = obj["features"][x]["geometry"]["coordinates"][y + 1]
+            cnext1 = obj["features"][x]["geometry"]["coordinates"][y + 2]
+            angle = angle3pt(cords, cnext, cnext1)
+            if not skip:
+                nProps["magnitudes"].append(obj["features"][x]["properties"]["magnitudes"][y])
+                nProps["directions"].append(obj["features"][x]["properties"]["directions"][y])
+                nProps["point_levels"].append(obj["features"][x]["properties"]["point_levels"][y])
+                feat["geometry"]["coordinates"].append(cords)
+            skip = (angle > 270)
+        if not skip:
+            nProps["magnitudes"].append(obj["features"][x]["properties"]["magnitudes"][m1+1])
+            nProps["directions"].append(obj["features"][x]["properties"]["directions"][m1+1])
+            nProps["point_levels"].append(obj["features"][x]["properties"]["point_levels"][m1+1])
+            feat["geometry"]["coordinates"].append(obj["features"][x]["geometry"]["coordinates"][m1+1])
+        feat["geometry"]["properties"] = nProps
+        new_obj["features"].append(feat)
+    return new_obj
+
+
+with open("test.geojson") as i:
+    data = json.loads(i.read())
+    data1 = json_filter(data)
+    with open("test1.geojson", "w") as o:
+        o.write(json.dumps(data1, indent=4))
